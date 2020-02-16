d9d8882e9d37bad1a23038c93f9f38f804ea8c6d
Vanege <11367082+Vanege@users.noreply.github.com>
2020-02-16 23:06:56 +0100

    styling of matched words

--- components/SearchBlock.vue
+++ components/SearchBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- TODO: make the material design icon work   prepend-icon="search" -->
-    Type an English or Globasa word to find below!
+    Type an English or a Globasa word
     <v-text-field
       v-model="searchedText"
       outlined
@@ -10,20 +10,49 @@
     <v-row>
       <v-col>
         <template v-if="hasEnglishMatches && searchedText.length > 2">
-          Found in English:
-          <div v-for="(match, index) in englishMatches" :key="`english-match-${index}`">
-            <template v-if="index < matchesMax">
-              <b>{{ match.eng }}</b> | {{ match.word }} | {{ match.leksiliAsel }}
-            </template>
+          <div class="english-block">
+            Found in English:
+            <div class="results-block">
+              <div v-for="(match, index) in englishMatches" :key="`english-match-${index}`">
+                <template v-if="index < matchesMax">
+                  <div class="result-block">
+                    <div class="eng-text">
+                      {{ match.eng }}
+                    </div>
+                    <div class="word-text">
+                      {{ match.word }}
+                    </div>
+                    <div class="leksili-asel-text">
+                      {{ match.leksiliAsel }}
+                    </div>
+                  </div>
+                </template>
+              </div>
+            </div>
           </div>
         </template>
       </v-col>
       <v-col>
         <template v-if="hasGlobasaMatches && searchedText.length > 2">
-          <div v-for="(match, index) in globasaMatches" :key="`globasa-match-${index}`">
-            <template v-if="index < matchesMax">
-              {{ match.eng }} | <b>{{ match.word }}</b> | {{ match.leksiliAsel }}
-            </template>
+          <div class="globasa-block">
+            Found in Globasa:
+            <div class="results-block">
+              <div v-for="(match, index) in globasaMatches" :key="`english-match-${index}`">
+                <template v-if="index < matchesMax">
+                  <div class="result-block">
+                    <div class="eng-text">
+                      {{ match.eng }}
+                    </div>
+                    <div class="word-text">
+                      {{ match.word }}
+                    </div>
+                    <div class="leksili-asel-text">
+                      {{ match.leksiliAsel }}
+                    </div>
+                  </div>
+                </template>
+              </div>
+            </div>
           </div>
         </template>
       </v-col>
@@ -53,4 +82,36 @@ export default class SearchBlock extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.english-block {
+  background-color: rgb(230, 230, 230);
+  border-radius: 5px;
+  padding: 10px;
+}
+.globasa-block {
+  background-color: rgb(210, 214, 255);
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.results-block {
+  margin-top: 5px;
+  margin-left: 10px;
+}
+
+.result-block {
+  margin-top: 10px;
+}
+
+.eng-text {
+  font-weight: 500;
+}
+.word-text {
+  margin-left: 15px;
+  font-style: italic;
+  font-weight: 500;
+}
+.leksili-asel-text {
+  margin-left: 15px;
+  font-weight: 300;
+}
 </style>
